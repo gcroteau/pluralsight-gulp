96cff1648d3d289660489a17b10fc4170d6b2aae
gcroteau <guillaume.croteau.1@gmail.com>
2015-10-12 00:21:50 -0400

    Added css compilation tasks. Added first watcher. Added prefixer and plumber plugins.

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
-var gulp = require('gulp');
 var args = require('yargs').argv;
+var gulp = require('gulp');
+var del = require('del');
 var $ = require('gulp-load-plugins')({lazy: true});
 
 var config = require('./gulp.config')();
@@ -14,6 +15,31 @@ gulp.task('vet', function() {
     .pipe($.jshint.reporter('jshint-stylish', {verbose: true}))
     .pipe($.jshint.reporter('fail'));
 });
+
+gulp.task('styles', function() {
+  log('Compiling Less --> CSS');
+
+  return gulp
+    .src(config.less)
+    .pipe($.plumber())
+    .pipe($.less())
+    .pipe($.autoprefixer({browsers: ['last 2 versions', '> 5%']}))
+    .pipe(gulp.dest(config.temp));
+});
+
+gulp.task('clean-styles', function(done) {
+  var files = config.temp + '**/*.css';
+  clean(files, done);
+});
+
+gulp.task('less-watchers', function() {
+  gulp.watch([config.less], ['styles']);
+});
+
+function clean(path, done) {
+  log('Cleaning: ' + $.util.colors.blue(path));
+  del(path, done);
+};
 
 function log(msg) {
   if(typeof(msg) === 'object') {
